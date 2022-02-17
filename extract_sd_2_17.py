065567b8b2b9973ad6f2cf87e7979f95b626d27d
arpanmuk <arpanmuk@buffalo.edu>
2022-02-17 16:39:21 -0500

    change

--- extract_sd_2_17.py
+++ extract_sd_2_17.py
@@ -12,26 +12,41 @@ descs = ['shape_idx', 'curvedness', 'd_norm']
 stats = ['mean', 'std']
 attr = ['area', 'volume']
 
-inorganic_names = [files.split('_')[1] + files.split('_')[2]
-           for files in os.listdir(cxslist[0]) if 'inorganic' in files]
-inorganic_names = [c.split('.')[0] for c in inorganic_names]
-inorganic_names.sort()
+max_inorganic = max([len([files for files in os.listdir(cxslist_i) if
+                          'inorganic' in files]) for cxslist_i in cxslist])
+max_organic = max([len([files for files in os.listdir(cxslist_i) if
+                          'inorganic' not in files]) for cxslist_i in cxslist])
+# inorganic_names = [files.split('_')[1] + files.split('_')[2]
+#            for files in os.listdir(cxslist[0]) if 'inorganic' in files]
+# inorganic_names = [c.split('.')[0] for c in inorganic_names]
+# inorganic_names.sort()
+inorganic_names = ['inorganic_' + str(i) for i in range(max_inorganic)]
+organic_names = ['organic_' + str(i) for i in range(max_organic)]
 
 columns = []
 for col in inorganic_names:
     for desc in descs:
         for stat in stats:
-            columns.append(col+'_'+desc+'_'+stat)
+            columns.append(col + '_' + desc + '_' + stat)
+
+for col in organic_names:
+    for desc in descs:
+        for stat in stats:
+            columns.append(col + '_' + desc + '_' + stat)
 
 inds = []
 surfaces = []
-for cxsfolder in tqdm(cxslist):
-    ind = cxsfolder.split('/')[-1]
+for cxsfolder in tqdm(cxslist[:5]):
+    ind = cxsfolder.split('\\')[-1]
     inds.append(ind)
     cxsfiles = os.listdir(cxsfolder)
     cxsfiles.sort()
-    inorganics = [os.path.join(cxsfolder, cxs) for cxs in cxsfiles if 'inorganic' in cxs]
-    inorganics.sort()
+    inorganics = len([files for files in cxsfiles if
+                          'inorganic' in files])
+    inorganic_names = ['inorganic_' + str(i) for i in range(inorganics)]
+    inorganics = [os.path.join(cxsfolder, ind + '_inorganic_' + str(i) + '.cxs')
+                  for i in range(inorganics)]
+
     surface_prop = {}
     for c_file, inorg in zip(inorganics, inorganic_names):
         hs = HS(c_file)
@@ -40,12 +55,17 @@ for cxsfolder in tqdm(cxslist):
                 func = getattr(np, stat)
                 surface_prop[inorg+'_' + desc + '_' + stat] = func(getattr(hs, desc))
 
-    organics = [os.path.join(cxsfolder, cxs) for cxs in cxsfiles if 'inorganic' not in cxs][0]
-    hs = HS(organics)
-    for desc in descs:
-        for stat in stats:
-            func = getattr(np, stat)
-            surface_prop['organic_' + desc + '_' + stat] = func(getattr(hs, desc))
+    organics = len([files for files in cxsfiles if
+                      'inorganic' not in files])
+    organic_names = ['organic_' + str(i) for i in range(organics)]
+    organics = [os.path.join(cxsfolder, ind + '_organic_' + str(i) + '.cxs')
+                for i in range(organics)]
+    for c_file, inorg in zip(organics, organic_names):
+        hs = HS(c_file)
+        for desc in descs:
+            for stat in stats:
+                func = getattr(np, stat)
+                surface_prop[inorg + '_' + desc + '_' + stat] = func(getattr(hs, desc))
     surfaces.append(surface_prop)
 
 import pandas as pd
